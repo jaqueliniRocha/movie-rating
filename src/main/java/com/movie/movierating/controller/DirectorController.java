786005918d0ab41fc91f0ab564bf8343bd43042f
jaqueliniRocha <jaquelini.rocha@hotmail.com>
2021-02-11 18:18:29 -0300

    50I2oMIa: directors listing

--- src/main/java/com/movie/movierating/controller/DirectorController.java
+++ src/main/java/com/movie/movierating/controller/DirectorController.java
@@ -1,12 +1,16 @@
 package com.movie.movierating.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.movie.movierating.model.Director;
 import com.movie.movierating.model.DirectorRepository;
+
 
 @CrossOrigin
 @RestController
@@ -17,6 +21,11 @@ public class DirectorController {
 		
 	DirectorController(DirectorRepository repository) {
 		this.repository = repository;
+	}
+	
+	@GetMapping("/director")
+	List<Director> all(){
+		return repository.findAll();
 	}
 	
 	@PostMapping("/director")
