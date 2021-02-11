0cca420c57c3f3a66e064c6bb195198f6bccef7d
jaqueliniRocha <jaquelini.rocha@hotmail.com>
2021-02-11 15:00:31 -0300

    A73Nzsk5 - entities created: movie, director and genre

---
+++ src/main/java/com/movie/movierating/model/GenreEnum.java
@@ -0,0 +1,19 @@
+package com.movie.movierating.model;
+
+public enum GenreEnum {
+	
+	ADVENTURE,
+	ACTION,
+	CONTEMPORATY,
+	COMEDY,
+	CRIME,
+	DRAMA,
+	FANTASY,
+	HORROR,
+	MYSTERY,
+	ROMANCE,
+	ANIMATION,
+	THRILLER,
+	HISTORICAL;
+
+}
---
+++ src/main/java/com/movie/movierating/model/Movie.java
@@ -0,0 +1,70 @@
+package com.movie.movierating.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String title;
+	private String year;
+	private int duration;
+	private double score;
+	
+	Directors director[];	
+	GenreEnum genre[];
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public Directors[] getDirector() {
+		return director;
+	}
+	public void setDirector(Directors[] director) {
+		this.director = director;
+	}
+	public GenreEnum[] getGenres() {
+		return genre;
+	}
+	public void setGenres(GenreEnum[] genres) {
+		this.genre = genres;
+	}
+	
+	
+	
+	
+}
